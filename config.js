9dd4f69c52662c348c5877fe5fe46a116fbbfbfe
John Makeev <makeev.john@gmail.com>
2017-06-15 00:29:17 +0700

    update

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  */
 
 
-module.exports.DB_URI = (process.env.MONGOLAB_URI || 'mongodb://localhost/firetour');
+module.exports.DB_URI = (process.env.MONGODB_URI || 'mongodb://localhost/firetour');
 module.exports.DB_POOL_SIZE = (process.env.DB_POOL_SIZE || 1);
